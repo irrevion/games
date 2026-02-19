882c50cba402d08338a6e6be707593d1eb67f280
Valentin Belousov <irrevion@gmail.com>
2026-02-20 00:50:58 +0400

    fix typo

--- app/messages/en-US/app.php
+++ app/messages/en-US/app.php
@@ -3,7 +3,7 @@
 return [
 	'404_title' => '404 - Page not found',
 	'404_headline' => '404 - Page not found',
-	'404_descr' => 'Well, that\'s sad... But it does\'t mean "GAME OVER".',
+	'404_descr' => 'Well, that\'s sad... But it doesn\'t mean "GAME OVER".',
 	'404_to_main' => 'Go to the main page.',
 	'404_feedback' => 'Let administrator know.',
 ];
